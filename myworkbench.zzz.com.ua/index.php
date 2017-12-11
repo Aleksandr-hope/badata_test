593fb9155291e7e6a27ebe612d0f0ea2e4ca95b1
Aleksandr <Aleksandr@Desktop-PC>
2017-12-11 12:51:26 +0200

    Initial commit.

---
+++ myworkbench.zzz.com.ua/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MyWorkbench</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Test for BAData">
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  </head>
+  <body>
+    <header>
+      <div class="centerAlign">
+        <h1>Welcome to MyWorkbench!</h1>
+      </div>
+    </header>
+    <section class="MainContainer">
+      <div class="centerAlign">
+
+      </div>
+    </section>
+
+    <!--SCRIPTS-->
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
